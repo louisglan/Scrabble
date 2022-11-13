69485d229ea057ccd38952b36a2ec056ed11a445
louisglan <louissam@hotmail.co.uk>
2022-11-13 22:16:36 +0000

    completed all challenges

--- Scrabble.js
+++ Scrabble.js
@@ -1,9 +1,8 @@
 // Plan: first create a tile class. This will have letter and points keys.
 // Create a function that uses this class to create each tile, then create duplicates of tiles that have duplicates
-import shuffle from "./shuffle.js";
-const dict = await Deno.readTextFile("./Scrabble/dictionary.txt"); //This isn't working???
+const dict = await Deno.readTextFile("./dictionary.txt"); //This isn't working???
 
-class Tile {
+export class Tile {
   constructor(letter) {
     this.letter = letter;
     this.points = this.pointsFor();
@@ -27,82 +26,179 @@ class Tile {
   }
 }
 
-export function createAllTiles() {
-  let allTiles = [];
-  const numberOfTiles = {
-    1: ["K", "J", "X", "Q", "Z"],
-    2: ["B", "C", "M", "P", "F", "H", "V", "W", "Y"],
-    3: ["G"],
-    4: ["L", "S", "U", "D"],
-    6: ["N", "R", "T"],
-    8: ["O"],
-    9: ["A", "I"],
-    12: ["E"],
-  };
-  for (let i = 0; i < Object.keys(numberOfTiles).length; i++) {
-    for (let j = 0; j < Number(Object.keys(numberOfTiles)[i]); j++) {
-      Object.values(numberOfTiles)[i].forEach((letter) => {
-        const tile = new Tile(letter);
-        allTiles.push(tile);
-      });
-    }
+export class AllTiles {
+  constructor() {
+    this.tiles = this.createAllTiles();
   }
-  return allTiles;
-}
 
-export function calculateScore(word) {
-  return word.reduce((cum, letter) => {
-    const value = letter.points;
-    return cum + value;
-  }, 0);
-}
-
-export function createHand(Tiles) {
-  const hand = [];
-  for (let i = 0; i < 7; i++) {
-    hand.push(Tiles.shift());
-  }
-  return hand;
-}
-
-export function findValidWord(hand, dict) {
-  const dictArray = dict.split("\n");
-  hand.forEach((tile, el) => {
-    tile.index = el;
-  });
-  filtered_dict = dict.filter((word) => {
-    word.length <= 7;
-  });
-  const twoLetterWords = [];
-  for (let i = 0; i < hand.length; i++) {
-    for (let j = 0; j < hand.length; j++) {
-      if (i != j) {
-        twoLetterWords.push([hand[i], hand[j]]);
+  createAllTiles() {
+    let allTiles = [];
+    const numberOfTiles = {
+      1: ["K", "J", "X", "Q", "Z"],
+      2: ["B", "C", "M", "P", "F", "H", "V", "W", "Y"],
+      3: ["G"],
+      4: ["L", "S", "U", "D"],
+      6: ["N", "R", "T"],
+      8: ["O"],
+      9: ["A", "I"],
+      12: ["E"],
+    };
+    for (let i = 0; i < Object.keys(numberOfTiles).length; i++) {
+      for (let j = 0; j < Number(Object.keys(numberOfTiles)[i]); j++) {
+        Object.values(numberOfTiles)[i].forEach((letter) => {
+          const tile = new Tile(letter);
+          allTiles.push(tile);
+        });
       }
     }
-  }
-  const threeLetterWords = [];
-  for (let i = 0; i < twoLetterWords.length; i++) {
-    for (let j = 0; j < hand.length; j++) {
-      if (
-        twoLetterWords[i][0].index != hand[j].index &&
-        twoLetterWords[i][1].index != hand[j].index
-      )
-        threeLetterWords.push(twoLetterWords[i].push(hand[j]));
-      threeLetterWords.push(twoLetterWords[i].unshift(hand[j]));
-    }
+    return allTiles;
   }
 
-  //and so on, then check dictionary for all words
-  return dictArray;
+  createHand() {
+    const hand = [];
+    for (let i = 0; i < 7; i++) {
+      hand.push(
+        this.tiles.splice(Math.floor(Math.random() * this.tiles.length), 1)[0]
+      );
+    }
+    return hand;
+  }
+}
+
+export class Hand {
+  constructor(hand) {
+    this.hand = hand;
+    this.newDict = dict.split("\n").filter((word) => word.length <= 7);
+  }
+
+  get letters() {
+    return this.hand.map((tile) => tile.letter);
+  }
+
+  findValidWordV2() {
+    const matchIndices = [];
+    const newDict = dict.split("\n").filter((word) => word.length <= 7);
+    for (let i = 0; i < newDict.length; i++) {
+      const lettersCopy = [...this.letters];
+      newDict[i].split("").forEach((dictLetter) => {
+        if (lettersCopy.includes(dictLetter.toUpperCase())) {
+          const index = lettersCopy.indexOf(dictLetter.toUpperCase());
+          lettersCopy.splice(index, 1);
+        }
+      });
+      if (newDict[i].length === 7 - lettersCopy.length) {
+        matchIndices.push(i);
+      }
+    }
+    return matchIndices;
+  }
+
+  get words() {
+    return this.findValidWordV2().map((index) => this.newDict[index]);
+  }
+
+  get tileWords() {
+    const tileWords = [];
+    for (const match of this.words) {
+      const currentMatch = [];
+      match
+        .toUpperCase()
+        .split("")
+        .forEach((letter) => {
+          const tile = this.hand.find((tile) => {
+            return tile.letter === letter;
+          });
+          currentMatch.push(tile);
+        });
+      tileWords.push(currentMatch);
+    }
+    return tileWords;
+  }
+
+  get lengths() {
+    return this.words.map((word) => word.length);
+  }
+
+  get longestWord() {
+    let maxLength = 0;
+    let index = 0;
+    this.lengths.forEach((length, i) => {
+      if (length > maxLength) {
+        console.log(index);
+        index = i;
+        maxLength = length;
+      }
+    });
+    return this.words[index];
+  }
+
+  get scores() {
+    const scores = [];
+    this.tileWords.forEach((word) => {
+      scores.push(
+        word.reduce((cum, value) => {
+          return cum + value.points;
+        }, 0)
+      );
+    });
+    return scores;
+  }
+
+  get scoresUsingHighestScoringTriple() {
+    const scores = [];
+    this.tileWords.forEach((word) => {
+      const maxPointsForLetter = word.reduce((cum, val) => {
+        if (val.points > cum) {
+          return val.points;
+        } else {
+          return cum;
+        }
+      }, 0);
+      const highestPointLetter = word.find((letter) => {
+        return letter.points === maxPointsForLetter;
+      });
+      scores.push(
+        word.reduce((cum, value) => {
+          return cum + value.points;
+        }, 0) +
+          highestPointLetter.points * 2
+      );
+    });
+    return scores;
+  }
+
+  findHighestScore(scores, words) {
+    let maxScore = 0;
+    let index = 0;
+    scores.forEach((score, i) => {
+      if (score > maxScore) {
+        index = i;
+        maxScore = score;
+      }
+    });
+    return words[index];
+  }
+
+  get highestScoringWord() {
+    return this.findHighestScore(this.scores, this.words);
+  }
+
+  get highestScoringWordWithTriple() {
+    return this.findHighestScore(
+      this.scoresUsingHighestScoringTriple,
+      this.words
+    );
+  }
 }
 
 export function main() {
-  const allTiles = createAllTiles();
-  const shuffledTiles = shuffle(allTiles);
-  const player1Hand = createHand(shuffledTiles);
-  const validWord = findValidWord(player1Hand, dict);
-  console.log(validWord);
+  const allTiles = new AllTiles();
+  const player1 = new Hand(allTiles.createHand());
+  console.log(`
+  Tiles: ${player1.letters}\n
+  Longest word: ${player1.longestWord}\n
+  Highest scoring word: ${player1.highestScoringWord}\n
+  Highest scoring word with triple letter: ${player1.highestScoringWordWithTriple}`);
 }
 
 main();
